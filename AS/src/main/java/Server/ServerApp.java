49c579bef5f1569d89b71eeaa570443f00a9f97d
huangsheng <2334170767@qq.com>
2022-05-24 16:02:19 +0800

    AS提交

---
+++ AS/src/main/java/Server/ServerApp.java
@@ -0,0 +1,17 @@
+package Server;
+
+import Server.Show.Starter;
+import javafx.application.Application;
+import Server.AS_Server;
+
+import java.sql.*;
+
+
+public class ServerApp {
+
+    public static void main(String[] args) {
+        //main启动starter
+        Application.launch(Starter.class);
+
+    }
+}
